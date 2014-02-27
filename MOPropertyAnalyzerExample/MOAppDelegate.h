83929bb69dada8a50a847719e2de544ee7bd6c06
Maciej Oczko <maciek.oczko@polidea.com>
2014-02-27 21:42:27 +0100

    Initial Commit

---
+++ MOPropertyAnalyzerExample/MOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MOAppDelegate.h
+//  MOPropertyAnalyzerExample
+//
+//  Created by Maciej Oczko on 27.02.2014.
+//  Copyright (c) 2014 Maciej Oczko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MOPropertyAnalyzerExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MOPropertyAnalyzerExample
+//
+//  Created by Maciej Oczko on 27.02.2014.
+//  Copyright (c) 2014 Maciej Oczko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MOAppDelegate class]));
+    }
+}
